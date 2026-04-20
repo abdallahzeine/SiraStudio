01ff32976c981096bff8c4f7cfb51ad167cc6206
Abdallah <abdallah@cv-maker.local>
2026-04-20 06:19:50 +0300

    re

--- SiraStudio/src/main.tsx
+++ SiraStudio/src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Analytics } from '@vercel/analytics/react'
 import './index.css'
 import App from './App.tsx'
 import { createCVStore, CVStoreProvider } from './store'
@@ -22,6 +23,7 @@ createRoot(document.getElementById('root')!).render(
     <CVStoreProvider store={store}>
       <EditorProvider>
         <App />
+        <Analytics />
       </EditorProvider>
     </CVStoreProvider>
   </StrictMode>,
